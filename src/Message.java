1ff4d6898cae55a4ec32e1213d78553538e327ee
MagriniLorenzo <138790523+MagriniLorenzo@users.noreply.github.com>
2024-09-16 17:47:36 +0200

    Classi Message e Publisher
    Creazione ed implementazione iniziale delle classi Message e Publisher

--- src/Message.java
+++ src/Message.java
@@ -1,14 +1,31 @@
+import java.time.LocalDateTime;
+
 public class Message {
-    private String text;
+    private static int idCounter = 0;
     private int id;
-    public Message(String text, int id){
-        this.text = text;
-        this.id = id;
+    private String testo;
+    private LocalDateTime dataInvio;
+
+    public Messaggio(String testo) {
+        this.id = ++idCounter;
+        this.testo = testo;
+        this.dataInvio = LocalDateTime.now();
     }
-    public String getText(){
-        return text;
-    }
-    public int getID(){
+
+    public int getId() {
         return id;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public LocalDateTime getDataInvio() {
+        return dataInvio;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nTesto: " + testo + "\nData: " + dataInvio;
     }
 }
--- src/Publisher.java
+++ src/Publisher.java
@@ -1,10 +1,26 @@
 public class Publisher {
     private Topic topic;
 
-    public Publisher() {
+    public Publisher(Topic topic) {
+        this.topic = topic;
     }
 
     public Topic getTopic() {
         return topic;
+    }
+
+    // Invia un messaggio al server attraverso il client
+    public void inviaMessaggio(String testoMessaggio, PrintWriter output) {
+        output.println("send " + topic.getNome() + " " + testoMessaggio);
+    }
+
+    // Richiede la lista dei messaggi inviati da questo publisher su questo topic
+    public void richiediMessaggiPersonali(PrintWriter output) {
+        output.println("list " + topic.getNome());
+    }
+
+    // Richiede la lista di tutti i messaggi inviati su questo topic
+    public void richiediMessaggiTotali(PrintWriter output) {
+        output.println("listall " + topic.getNome());
     }
 }
